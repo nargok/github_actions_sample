61756b02731158db7bdb6c6c9d043692045fbcb1
nargok <nargok@gmail.com>
2020-09-20 13:55:13 +0900

    fizzbuzz test

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = function fizzbuz(value) {
+  if (value % 15 === 0) {
+    return "FizzBuzz";
+  } else if (value % 3 === 0) {
+    return "Fizz";
+  } else if (value % 5 === 0) {
+    return "Buzz";
+  }
+  return String(value);
+};
